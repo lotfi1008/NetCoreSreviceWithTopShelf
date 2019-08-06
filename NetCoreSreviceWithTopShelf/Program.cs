b63f4acff8cc19a5c4a7b1267c0a8ca671fb961d
mohammad lotfi <lotfi.engin@gmail.com>
2019-08-06 14:34:04 +0430

    Add project files.

---
+++ NetCoreSreviceWithTopShelf/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Topshelf;
+
+namespace NetCoreSreviceWithTopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HostFactory.Run(windowsService =>
+            {
+                windowsService.Service<TrakDateTime>(c=> {
+                    c.ConstructUsing(x =>new TrakDateTime());
+                    c.WhenStarted(service => service.Start());
+                    c.WhenStopped(service => service.Stop());
+                });
+                
+                windowsService.RunAsLocalSystem();
+                windowsService.StartAutomatically();
+
+                windowsService.SetDescription("NetCoreSreviceWithTopShelfDescription");
+                windowsService.SetDisplayName("NetCoreSreviceWithTopShelfDisplayName");
+                windowsService.SetServiceName("NetCoreSreviceWithTopShelf");
+            });
+
+           
+        }
+
+
+
+
+
+
+        public class TrakDateTime
+        {
+            public TrakDateTime()
+            {
+                
+            }
+            public void Start()
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                for (int i = 0; i < 100; i++)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    System.IO.File.AppendAllLines(@"C://Temp/sample.txt", new string[] { DateTime.Now.ToString() });
+                    Console.WriteLine("[Go] : " + DateTime.Now.ToString());
+                }
+            }
+            public void Stop()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[Go] : [Done]");
+            }
+        }
+    }
+}
